3a30bb6a678b5201a4fb7aa6bec3b2d44ccfd0fb
HAWKCAN <farizrahman335577@gmail.com>
2025-12-10 23:41:23 +0700

    fix

--- backend/api/auth/pilih-side.php
+++ backend/api/auth/pilih-side.php
@@ -3,10 +3,14 @@ require_once __DIR__ . '/../../config/database.php';
 
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST, OPTIONS');
+header('Access-Control-Allow-Methods: POST,GET, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
 
 // preflight
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(204);
     exit;
