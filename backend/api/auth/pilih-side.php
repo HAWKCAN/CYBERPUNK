d6e7993cb01b0533b454af372359aa0a1cda90a4
ferdiansamp <mohammad.samputra@mhs.unsoed.ac.id>
2025-12-11 00:33:07 +0700

    push side2

--- backend/api/auth/pilih-side.php
+++ backend/api/auth/pilih-side.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/../../config/database.php';
 
 header('Content-Type: application/json');
-header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Origin: http://127.0.0.1:5500');
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 
